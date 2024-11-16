1aa75b8ccd44cc8101daa33c83fef821ee289103
mmcmu <gabrielacalistavaniaputri_2226240062@mhs.mdp.ac.id>
2024-11-16 11:08:36 +0700

    update

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/signin_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +14,24 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wisata Candi',
-      theme: ThemeData( 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
-      ),
-      home: DetailScreen(
-        candi: candiList[0],
-        ),
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.deepPurple),
+            titleTextStyle: TextStyle(
+              color: Colors.deepPurple,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+            primary: Colors.deepPurple,
+            surface: Colors.deepPurple[50],
+          ),
+          useMaterial3: true),
+      //home: DetailScreen(candi: candiList[0],),
+      //home: ProfileScreen(),
+      home: SignInScreen(),
     );
   }
 }
